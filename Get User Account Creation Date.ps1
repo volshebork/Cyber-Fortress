6fcd1f80f934b52ae3c8b1cef2b8e8175500beb3
volshebork <oscar.j.cortez1@pm.me>
2025-08-08 15:14:20 -0400

    Update Get User Account Creation Date.ps1

--- Get User Account Creation Date.ps1
+++ Get User Account Creation Date.ps1
@@ -1,3 +1,5 @@
 # Get the date a specified samaccountname was made.
+# <samaccountname> is a placeholder. It needs to be replaced with the actual samaccountname.
 Get-adUser -identity <samaccountname> -properties whencreated |
+
 select samaccountname, whencreated
